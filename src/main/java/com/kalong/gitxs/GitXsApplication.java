b75b9d0321d013bf6f841dd56ebb263323479ab8
billsikalong <bb123bill@gmail.com>
2023-04-26 22:55:15 +0800

    第一次提交(這裡通常寫更新了什麼如創建了.JAVA文件)

---
+++ src/main/java/com/kalong/gitxs/GitXsApplication.java
@@ -0,0 +1,13 @@
+package com.kalong.gitxs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitXsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitXsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kalong/gitxs/GitXsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalong.gitxs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitXsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
